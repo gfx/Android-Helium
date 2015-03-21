ac2672b6188321a3c3ce1d3af0e0b916bb1e9473
FUJI Goro (gfx) <gfuji@cpan.org>
2015-03-21 09:35:38 +0900

    remove debug output

--- app/src/test/java/com/github/gfx/helium/UserAgentTest.java
+++ app/src/test/java/com/github/gfx/helium/UserAgentTest.java
@@ -9,7 +9,6 @@ import static org.hamcrest.MatcherAssert.*;
 public class UserAgentTest {
     @Test
     public void testUserAgent() {
-        System.out.println("XXX " + UserAgent.build());
         assertThat(UserAgent.build(), startsWith(BuildConfig.APPLICATION_ID));
     }
 }
