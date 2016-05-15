c7a1c9f53a07949615c99cac0339618d9e5d4117
FUJI Goro (gfx) <gfuji@cpan.org>
2016-05-15 18:38:35 +0900

    use OkHttp3 for glide

--- app/src/release/java/com/github/gfx/helium/StethoDelegator.java
+++ app/src/release/java/com/github/gfx/helium/StethoDelegator.java
@@ -2,9 +2,11 @@ package com.github.gfx.helium;
 
 import okhttp3.Interceptor;
 
+import android.app.Application;
+
 public class StethoDelegator {
 
-    public static void setup() {
+    public static void setup(Application application) {
     }
 
     public static Interceptor createNetworkInterceptor() {
